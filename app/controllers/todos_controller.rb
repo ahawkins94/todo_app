53ae80d0efc35b5085c8e4370d8e6abbcc89d146
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-10-05 17:23:49 +0100

    index now showing todos though there are none yet woop

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,3 +1,45 @@
 class TodosController < ApplicationController
+    def index
+    @todos = current_user.todos
+  end
 
+  def show
+    @todo = current_user.todos.find(params[:id])
+
+  end
+
+  def new
+    @todo = current_user.todos.new
+  end
+
+  def create
+
+    new_todo = current_user.todos.create(book_params)
+    new_todo.save
+
+    redirect_to new_todo
+
+  end
+
+  def edit
+    @todo = current_user.todos.find(params[:id])
+  end
+
+  def update
+
+    todo = current_user.todos.find(params[:id])
+    todo.update(todo_params)
+
+    redirect_to todo
+  end
+
+  def destroy
+    current_user.todos.destroy(params[:id])
+    redirect_to todos_url
+  end
+
+  protected
+  def book_params
+    params.require(:todo).permit(:title, :item)
+  end
 end
